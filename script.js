ea3f7e8466d34ae31fff514a985ba2b03ebc9cf2
GABRIEL APOLINARIO LOPES <gabi.app.lopes@gmail.com>
2026-03-30 21:38:40 -0300

    calculadora-emdias

--- script.js
+++ script.js
@@ -9,9 +9,20 @@
      */
     function resolverDesafio() {
         const outputDiv = document.getElementById('output');
-        
+
         // --- SEU CÓDIGO COMEÇA AQUI ---
-        
+        let n1 = parseFloat( document.getElementById("n1").value)
+        let n2 = parseFloat( document.getElementById("n2").value)
+        let n3 = parseFloat( document.getElementById("n3").value)
+        let n4 = parseFloat( document.getElementById("n4").value)
+
+        let media = (n1 + n2 + n3 + n4) / 4
+
+        if (media >= 7) {
+            outputDiv.innerHTML = " Aprovado com sucesso! Média:" + media
+        } else {
+            outputDiv.innerHTML = " Reprovado com sucesso! Média:" + media
+        }
         // Exemplo de como capturar:
         // let n1 = parseFloat(document.getElementById('n1').value);
 
